bbcc4cef27476950f47efdd7bbb6e16265eefa08
Renan Dincer <renandincer@gmail.com>
2016-01-11 21:02:13 -0500

    added functionality to accurately represent xml files

--- config.rb
+++ config.rb
@@ -1,6 +1,6 @@
 require 'json'
 require 'net/http'
-
+require 'nokogiri'
 
 ###
 # Compass
@@ -68,7 +68,48 @@ page "*.md", :layout => "markdown"
 # Methods defined in the helpers block are available in templates
  helpers do
    def generatefeed(program)
-     "kk"
+     feed = Nokogiri::XML::Builder.new do |xml|
+			 xml.rss('xmlns:itunes' => "http://www.itunes.com/dtds/podcast-1.0.dtd") do
+				 xml.channel do
+				   xml.title program["name"]
+				   xml.link "http://wjrh.org/#{program["shortname"]}"
+					 xml.copyright program["copyright"]
+					 xml['itunes'].subtitle program["subtitle"]
+					 xml['itunes'].author program["creators"].join(", ")
+					 if not program['description'].nil?
+						 xml['itunes'].summary {
+							 xml.cdata Tilt['markdown'].new { program['description'] }.render
+						 }
+						 xml.description {
+							 xml.cdata Tilt['markdown'].new { program['description'] }.render
+						 }
+					 end
+					 xml['itunes'].image('href' => "http://wjrh.org/vbb/logo2.jpg")
+					 program['episodes'].each do |episode|
+						 xml.item do
+							 xml.title episode['name']
+							 xml['itunes'].author program['creators'].join(", ")
+							 if not episode['description'].nil?
+						 		 xml['itunes'].subtitle {
+									xml.cdata Tilt['markdown'].new { episode['description'] }.render
+						 	 	 }
+						 		 xml['itunes'].summary {
+							 		xml.cdata Tilt['markdown'].new { episode['description'] }.render
+							   }
+					 		 end
+							 xml['itunes'].image("href" => episode['image'])
+							 episode["medias"].each do |media|
+								 xml.enclosure("url" => media["url"], "length" => media["length"], "type" => media["type"])
+							 end
+							 xml.guid episode["guid"] ||= episode["id"]
+							 xml.pubDate Time.parse(episode["pubdate"]).rfc2822
+							 xml['itunes'].duration episode['medias'][0]['length']
+						 end
+					 end 
+				 end
+			 end
+		 end
+		 return feed.to_xml
    end
  end
 
